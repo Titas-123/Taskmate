3c4427dc8d05e3ad9b88980599ca0f84651144f3
Titas Das <dastitas20@gmail.com>
2022-03-08 01:33:10 +0530

    Fourth Commit

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -113,6 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -126,3 +128,4 @@ LOGIN_URL = "login"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
