b38cd4d9748acabe2f3fa68d2000776bb5872ccf
Titas Das <dastitas20@gmail.com>
2022-03-08 01:25:41 +0530

    Third Commit

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,6 +1,8 @@
-from pathlib import Path
+import django_heroku
 import os
+from pathlib import Path
 import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,5 @@ LOGIN_URL = "login"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
